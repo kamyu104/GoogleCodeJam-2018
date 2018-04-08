1e8c931dc961159fd0a179ca20272e221313967d
Allen Liu <kamyu104@gmail.com>
2018-04-08 11:49:58 +0800

    Update solution

--- Qualification Round/saving-the-universe-again.py
+++ Qualification Round/saving-the-universe-again.py
@@ -3,38 +3,41 @@
 # Google Code Jam 2018 Qualification Round - Problem A. Saving The Universe Again
 # https://codejam.withgoogle.com/2018/challenges/00000000000000cb/dashboard
 #
-# Time:  O(SlogS), S is the count of 'S'
-# Space: O(S)
+# Time:  O(P)
+# Space: O(P)
 #
 
-import heapq
+import collections
 
-def parse(P):
-    max_heap, count = [], 0
+def count(P):
+    exps, count = collections.defaultdict(int), 0
     for c in P:
         if c == 'C':
             count += 1
         else:
-            heapq.heappush(max_heap, -count)
-    return max_heap
+            exps[count] += 1
+    max_exp, damages = 0, 0
+    for exp, count in exps.iteritems():
+        max_exp = max(max_exp, exp)
+        damages += count*(2**exp)
+    for i in reversed(xrange(max_exp)):
+        exps[i] += exps[i+1]
+    return exps, max_exp, damages 
 
 def saving_the_universe_again():
     D, P = raw_input().strip().split()
     D = int(D)
-    max_heap = parse(P)
 
-    hacks, to_reduce = 0, sum(map(lambda(x): 2**-x, max_heap))-D
-    while max_heap:
+    exps, max_exp, damages = count(P)
+    hacks, to_reduce = 0, damages-D
+    while max_exp > 0:
         if to_reduce <= 0:
             break
-        count = heapq.heappop(max_heap)
-        if count == 0:
-            break
-        count += 1
-        to_reduce -= 2**-count
+        to_reduce -= 2**(max_exp-1)
+        exps[max_exp] -= 1
+        if exps[max_exp] == 0:
+            max_exp -= 1
         hacks += 1
-        if count:
-            heapq.heappush(max_heap, count)
 
     return hacks if to_reduce <= 0 else "IMPOSSIBLE"
 
