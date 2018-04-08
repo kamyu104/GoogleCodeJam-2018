5cfbaef6beb3bbe771fbbfe777a3e2a45a2ca3a3
Allen Liu <kamyu104@gmail.com>
2018-04-08 10:45:26 +0800

    Update Solutions

--- Qualification Round/trouble-sort.py
+++ Qualification Round/trouble-sort.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2018 kamyu. All rights reserved.
 #
 # Google Code Jam 2018 Qualification Round - Problem B. Trouble Sort
-# https://codejam.withgoogle.com/2018/challenges/00000000000000cb/dashboard
+# https://codejam.withgoogle.com/2018/challenges/00000000000000cb/dashboard/00000000000079cb
 #
 # Time:  O(NlogN)
 # Space: O(N)
