33ecdb01f0963eb603c6a8a2fd5dfcc7de70a984
kamyu <kamyu104@users.noreply.github.com>
2018-05-04 00:29:17 +0800

    Update transmutation2.cpp

--- Round 1B/transmutation2.cpp
+++ Round 1B/transmutation2.cpp
@@ -77,7 +77,7 @@ int64_t transmutation() {
         cin >> G[i];
     }
     int64_t left = G[0];
-    int64_t right = accumulate(G.cbegin(), G.cend(), 0LL);
+    int64_t right = accumulate(G.cbegin(), G.cend(), 0.0f);
     while (left <= right) {
         const auto mid = left + (right - left) / 2;
         if (impossible(mid, R, G)) {
