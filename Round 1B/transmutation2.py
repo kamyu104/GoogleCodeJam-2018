29aad371f8ef0c01001eb0a9cf3b0d86bf795962
kamyu <kamyu104@users.noreply.github.com>
2018-05-04 00:24:47 +0800

    Update transmutation2.py

--- Round 1B/transmutation2.py
+++ Round 1B/transmutation2.py
@@ -32,8 +32,7 @@ def impossible(L, R, G):
     i = 0
     while i != len(G):
         Ri = R[i]
-        if i in Ri or \
-           sum(G)-G[i] < -G[i]*sum(Ri):
+        if i in Ri:
             return True
         add(G, multiply(Ri, G[i]))
         G[i] = 0
