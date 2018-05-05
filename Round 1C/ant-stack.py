fab3dd4040f750b70483c9a8ac045291545442c0
Allen Liu <kamyu104@gmail.com>
2018-05-05 23:10:46 +0800

    Update

--- Round 1C/ant-stack.py
+++ Round 1C/ant-stack.py
@@ -1,10 +1,10 @@
 # Copyright (c) 2018 kamyu. All rights reserved.
 #
-# Google Code Jam 2018 Round 1C - Problem C. Ant Stack
-# https://codejam.withgoogle.com/2018/challenges/0000000000007765/dashboard/000000000003e0a8
+# Google Code Jam 2018 Round 1C - Problem A. A Whole New Word
+# https://codejam.withgoogle.com/2018/challenges/0000000000007765/dashboard
 #
-# Time:  O(N * K)
-# Space: O(K)
+# Time:  O(N * K), T is the number of nodes in trie
+# Space: O(T)
 #
 
 def get_upper_bound():
@@ -22,20 +22,19 @@ def get_upper_bound():
 def ant_stack(K):
     N = input()
     W = map(int, raw_input().strip().split())
-    INF = 10**14
     result = 1
-    dp = [INF for _ in xrange(2*(K+1))]
-    dp[0], dp[1] = 0, W[0]
+    dp = [[float("inf") for _ in xrange(K+1)] for _ in xrange(2)]
+    dp[0][0], dp[0][1] = 0, W[0]
     for i in xrange(1, N):
-        dp[(i%2)*K] = 0
+        dp[i%2][0] = 0
         for j in xrange(1, min(i+2, K+1)):
-            dp[(i%2)*K+j] = dp[((i-1)%2)*K+j]
-            if dp[((i-1)%2)*K+j-1] <= 6*W[i]:
-                dp[(i%2)*K+j] = min(dp[(i%2)*K+j], dp[((i-1)%2)*K+j-1]+W[i])
-            if dp[(i%2)*K+j] != INF:
+            dp[i%2][j] = dp[(i-1)%2][j]
+            if dp[(i-1)%2][j-1] <= 6*W[i]:
+                dp[i%2][j] = min(dp[i%2][j], dp[(i-1)%2][j-1]+W[i])
+            if dp[i%2][j] != float("inf"):
                 result = max(result, j)
     return result
 
-K = 139 # get_upper_bound()
+K = 139 #get_upper_bound()
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, ant_stack(K))
