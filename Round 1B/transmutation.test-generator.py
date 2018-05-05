63859141ece647e6fb2cb0fb659ed3052acaf273
Allen Liu <kamyu104@gmail.com>
2018-05-06 02:38:42 +0800

    Update

--- Round 1B/transmutation.test-generator.py
+++ Round 1B/transmutation.test-generator.py
@@ -8,7 +8,9 @@ for _ in xrange(T):
     M = random.randint(2, 100)
     print M
     for i in xrange(M):
-        print random.randint(1,M), random.randint(1,M)
+        R1 = random.randint(1,M-1)
+        R2 = random.randint(R1+1, M)
+        print R1, R2
     Gs = []
     for i in xrange(M):
         Gs.append(str(random.randint(0,G)))
