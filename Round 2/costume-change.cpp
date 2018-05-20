ae82ae61ca9b6caf432849b445acf82b27a9f7c5
Allen Liu <kamyu104@gmail.com>
2018-05-20 22:26:47 +0800

    Update costume-change.cpp

--- Round 2/costume-change.cpp
+++ Round 2/costume-change.cpp
@@ -25,61 +25,60 @@ bool dfs(const vector<vector<int>>& edge,
          int u,
          const vector<int>& level,
          vector<bool> *vis,
-         vector<int> *pr,
-         vector<int> *pr2) {
-	(*vis)[u] = true;
-	for (auto it = edge[u].begin(); it != edge[u].end(); ++it) {
-		int v = (*pr2)[*it];
-		if (v == -1 || (!(*vis)[v] && level[u] < level[v] &&
-                         dfs(edge, v, level, vis, pr, pr2))) {
-			(*pr)[u] = *it, (*pr2)[*it] = u;
-			return true;
-		}
-	}
-	return false;
+         vector<int> *pair1,
+         vector<int> *pair2) {
+    (*vis)[u] = true;
+    for (auto it = edge[u].begin(); it != edge[u].end(); ++it) {
+        int v = (*pair2)[*it];
+        if (v == -1 || (!(*vis)[v] && level[u] < level[v] &&
+                        dfs(edge, v, level, vis, pair1, pair2))) {
+            (*pair1)[u] = *it, (*pair2)[*it] = u;
+            return true;
+       }
+    }
+    return false;
 }
 
 int hopcroftKarp(const vector<vector<int>>& edge) {
-    const int n = edge.size();
+    const int n = edge.size();  // n is the number of left nodes
     vector<bool> vis(n);
     vector<int> level(n);
-    vector<int> pr(n, -1);
-    vector<int> pr2(n, -1);
-    
-	for (int match = 0; ;) {
-		queue<int> Q;
-		for (int i = 0; i < n; ++i) {
-			if (pr[i] == -1) {
-				level[i] = 0;
-				Q.emplace(i);
-			} else {
+    vector<int> pair1(n, -1);
+    vector<int> pair2(n, -1);
+    for (int match = 0; ;) {
+        queue<int> Q;
+        for (int i = 0; i < n; ++i) {
+            if (pair1[i] == -1) {
+                level[i] = 0;
+                Q.emplace(i);
+            } else {
                 level[i] = -1;
             }
-		}
-		while (!Q.empty()) {
-			int u = Q.front(); Q.pop();
-			for (auto it = edge[u].begin(); it != edge[u].end(); ++it) {
-				int v = pr2[*it];
-				if (v != -1 && level[v] < 0) {
-					level[v] = level[u] + 1;
-					Q.emplace(v);
-				}
-			}
-		}
-		for (int i = 0; i < n; ++i) {
+        }
+        while (!Q.empty()) {
+            int u = Q.front(); Q.pop();
+            for (auto it = edge[u].begin(); it != edge[u].end(); ++it) {
+                int v = pair2[*it];
+                if (v != -1 && level[v] < 0) {
+                    level[v] = level[u] + 1;
+                    Q.emplace(v);
+                }
+            }
+        }
+        for (int i = 0; i < n; ++i) {
             vis[i] = false;
         }
-		int d = 0;
-		for (int i = 0; i < n; ++i) {
-            if (pr[i] == -1 && dfs(edge, i, level, &vis, &pr, &pr2)) {
+        int d = 0;
+        for (int i = 0; i < n; ++i) {
+            if (pair1[i] == -1 && dfs(edge, i, level, &vis, &pair1, &pair2)) {
                 ++d;
             }
         }
-		if (d == 0) {
+        if (d == 0) {
             return match;
         }
-		match += d;
-	}
+        match += d;
+    }
 }
 
 int costume_change() {
@@ -93,11 +92,11 @@ int costume_change() {
     }
     int keep = 0;
     for (int color = -N; color <= N; ++color) {
-        vector<vector<int>> edge(2 * N);
+        vector<vector<int>> edge(N);
         for (int i = 0; i < N; ++i) {
             for (int j = 0; j < N; ++j) {
                 if (A[i][j] == color) {
-                    edge[i + N].emplace_back(j);
+                    edge[i].emplace_back(j);
                 }
             }
         }
