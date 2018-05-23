5a8bf096a0570211ad7abdb5eeffa9c7f7d84ea0
kamyu <kamyu104@users.noreply.github.com>
2018-05-23 13:57:29 +0800

    Update graceful-chainsaw-jugglers2.py

--- Round 2/graceful-chainsaw-jugglers2.py
+++ Round 2/graceful-chainsaw-jugglers2.py
@@ -16,17 +16,14 @@ def graceful_chainsaw_jugglers(dp):
     R, B = map(int, raw_input().strip().split())
     return dp[R][B]
 
-V = []
+dp = [[0 for _ in xrange(MAX_B+1)] for _ in xrange(MAX_R+1)]
 for i in xrange(MAX_R+1):
     for j in xrange(MAX_B+1):
-        if (i, j) == (0, 0):
+        if (i, j) == (0, 0) or \
+           not((j+1)*i*(i+1)//2 <= MAX_R and (i+1)*j*(j+1)//2 <= MAX_B):
             continue
-        if (j+1)*i*(i+1)//2 <= MAX_R and (i+1)*j*(j+1)//2 <= MAX_B:
-            V.append((i, j))
-dp = [[0 for _ in xrange(MAX_B+1)] for _ in xrange(MAX_R+1)]
-for i in xrange(len(V)):
-   for r in reversed(xrange(V[i][0], MAX_R+1)):
-       for b in reversed(xrange(V[i][1], MAX_B+1)):
-           dp[r][b] = max(dp[r][b], 1+dp[r-V[i][0]][b-V[i][1]])
+        for r in reversed(xrange(i, MAX_R+1)):
+               for b in reversed(xrange(j, MAX_B+1)):
+                   dp[r][b] = max(dp[r][b], 1+dp[r-i][b-j])
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, graceful_chainsaw_jugglers(dp))
