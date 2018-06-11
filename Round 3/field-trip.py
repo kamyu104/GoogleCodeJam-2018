795410151f11c61c4cb743360ba4b7467c682ad2
Allen Liu <kamyu104@gmail.com>
2018-06-11 22:13:29 +0800

    Create field-trip.py

---
+++ Round 3/field-trip.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2018 kamyu. All rights reserved.
+#
+# Google Code Jam 2018 Round 3 - Problem A. Field Trip
+# https://codejam.withgoogle.com/2018/challenges/0000000000007707/dashboard
+#
+# Time:  O(N)
+# Space: O(1)
+#
+
+def field_trip():
+    N = input()
+    R = [0]*N
+    C = [0]*N
+    for i in xrange(N):
+        R[i], C[i] = map(int, raw_input().strip().split())
+    return max(((max(R)-min(R)+1)//2, (max(C)-min(C)+1)//2))
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, field_trip())
