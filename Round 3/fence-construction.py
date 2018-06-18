c577fc54bd71d5525b77a87bc67fd2be262842c5
Allen Liu <kamyu104@gmail.com>
2018-06-19 00:17:36 +0800

    Update fence-construction.py

--- Round 3/fence-construction.py
+++ Round 3/fence-construction.py
@@ -18,8 +18,8 @@ def clockwiseangle_and_distance(point, origin):
     if lenvector == 0:
         return -math.pi, 0
     normalized = [vector[0]/lenvector, vector[1]/lenvector]
-    dotprod  = normalized[0]*refvec[0] + normalized[1]*refvec[1]
     diffprod = refvec[1]*normalized[0] - refvec[0]*normalized[1]
+    dotprod  = normalized[0]*refvec[0] + normalized[1]*refvec[1]
     angle = math.atan2(diffprod, dotprod)
     if angle < 0:
         return 2*math.pi+angle, lenvector
