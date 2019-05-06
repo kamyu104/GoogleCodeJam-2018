f6b9e7d91b5be49acb5f3dbcb6c58bc71a4934b3
kamyu <kamyu104@users.noreply.github.com>
2019-05-06 08:16:30 +0800

    Update ant-stack.cpp

--- Round 1C/ant-stack.cpp
+++ Round 1C/ant-stack.cpp
@@ -2,7 +2,7 @@
 
 /*
  * Google Code Jam 2018 Round 1C - Problem C. Ant Stack
- * https://codejam.withgoogle.com/2018/challenges/0000000000007765/dashboard/000000000003e0a8
+ * https://codingcompetitions.withgoogle.com/codejam/round/0000000000007765/000000000003e0a8
  *
  * Time  : O(N * K)
  * Space : O(K)
