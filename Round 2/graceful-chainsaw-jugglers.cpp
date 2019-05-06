113682d08a0d3ad254f91792f556fef9762301c6
kamyu <kamyu104@users.noreply.github.com>
2019-05-06 11:20:09 +0800

    Update graceful-chainsaw-jugglers.cpp

--- Round 2/graceful-chainsaw-jugglers.cpp
+++ Round 2/graceful-chainsaw-jugglers.cpp
@@ -5,7 +5,7 @@
  * https://codingcompetitions.withgoogle.com/codejam/round/0000000000007706/00000000000459f3
  *
  * Time:  O(R^(4/3)*B^(4/3))
- * Space: O(R^(4/3)*B^(4/3))
+ * Space: O(R * B)
  *
  */
 
