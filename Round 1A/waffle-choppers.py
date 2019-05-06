a729fb1c09688179070294491ea891f3d6b96066
kamyu <kamyu104@users.noreply.github.com>
2019-05-06 08:14:08 +0800

    Update waffle-choppers.py

--- Round 1A/waffle-choppers.py
+++ Round 1A/waffle-choppers.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2018 kamyu. All rights reserved.
 #
 # Google Code Jam 2018 Round 1A - Problem A. Waffle Choppers
-# https://codejam.withgoogle.com/2018/challenges/0000000000007883/dashboard
+# https://codingcompetitions.withgoogle.com/codejam/round/0000000000007883/000000000003005a
 #
 # Time:  O(R * C)
 # Space: O(R + C)
