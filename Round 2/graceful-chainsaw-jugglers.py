f575eb72af1464f922306334f1fa8e38dec65979
kamyu <kamyu104@users.noreply.github.com>
2019-05-06 08:38:13 +0800

    Update graceful-chainsaw-jugglers.py

--- Round 2/graceful-chainsaw-jugglers.py
+++ Round 2/graceful-chainsaw-jugglers.py
@@ -3,7 +3,7 @@
 # Google Code Jam 2018 Round 2 - Problem B. Graceful Chainsaw Jugglers
 # https://codingcompetitions.withgoogle.com/codejam/round/0000000000007706/00000000000459f3
 #
-# Time:  O(R^(4/3)*B^(4/3))
+# Time:  O(R^(4/3)*B^(4/3)), pass in PyPy2 but Python2
 # Space: O(R^(4/3)*B^(4/3))
 #
 
@@ -12,34 +12,25 @@ import sys
 
 MAX_R, MAX_B = 500, 500
 
-def memoization(V, i, r, b, lookup):
-    if r < 0 or b < 0:
-        return float("-inf")
-    if i < 0:
-        return 0
-    key = i*(MAX_R+1)*(MAX_B+1)+r*(MAX_B+1)+b
-    if lookup[key] is None:
-        lookup[key] = max(memoization(V, i-1, r, b, lookup),
-                          1 + memoization(V, i-1, r-V[i][0], b-V[i][1], lookup))
-    return lookup[key]
-
-def graceful_chainsaw_jugglers(V, lookup):
+def graceful_chainsaw_jugglers(dp):
     R, B = map(int, raw_input().strip().split())
-    return memoization(V, len(V)-1, R, B, lookup)
+    return dp[R][B]
 
 V = []
 for i in xrange(MAX_R+1):
-    if i*(i+1)//2 > MAX_R:
-        break
     for j in xrange(MAX_B+1):
-        if j*(j+1)//2 > MAX_B:
-            break
         if (i, j) == (0, 0):
             continue
-        if (j+1)*i*(i+1)//2 > MAX_R or (i+1)*j*(j+1)//2 > MAX_B:
-            break
-        V.append((i, j))
-lookup = [None] * (len(V)+1)*(MAX_R+1)*(MAX_B+1)
-sys.setrecursionlimit((len(V)+1)+(MAX_R+1)+(MAX_B+1))
+        if (j+1)*i*(i+1)//2 <= MAX_R and (i+1)*j*(j+1)//2 <= MAX_B:
+            V.append((i, j))
+dp = [[0 for _ in xrange(MAX_B+1)] for _ in xrange(MAX_R+1)]
+for i in xrange(len(V)):
+   for r in reversed(xrange(MAX_R+1)):
+       if r-V[i][0] < 0:
+           break
+       for b in reversed(xrange(MAX_B+1)):
+           if b-V[i][1] < 0:
+               break
+           dp[r][b] = max(dp[r][b], 1+dp[r-V[i][0]][b-V[i][1]])
 for case in xrange(input()):
-    print 'Case #%d: %s' % (case+1, graceful_chainsaw_jugglers(V, lookup))
+    print 'Case #%d: %s' % (case+1, graceful_chainsaw_jugglers(dp))
