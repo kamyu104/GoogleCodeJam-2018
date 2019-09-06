66f58c5a8a9fd367537b7cea2bbb488f9b8060be
Allen Liu <kamyu104@gmail.com>
2019-09-07 04:31:05 +0800

    update

---
+++ World Finals/jurisdiction-restrictions.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2019 kamyu. All rights reserved.
+#
+# Google Code Jam 2019 World Finals - Problem A. Jurisdiction Restrictions
+# https://codingcompetitions.withgoogle.com/codejam/round/0000000000007766/000000000004dbbd
+#
+# Time:  O(2^(2S)), pass in PyPy2 but Python2
+# Space: O(2^S)
+#
+
+def jurisdiction_restrictions():
+    R, C, S = map(int, raw_input().strip().split())
+    Rs, Cs, Ds = [0]*S, [0]*S, [0]*S
+    for i in xrange(S):
+        Rs[i], Cs[i], Ds[i] = map(int, raw_input().strip().split())
+        Rs[i] -= 1
+        Cs[i] -= 1
+
+    intersections = [0]*(2**S)
+    for i in xrange(1, len(intersections)):
+        min_r, max_r = 0, R-1
+        min_c, max_c = 0, C-1
+        for j in xrange(S):
+            if not (i & (1<<j)):
+                continue
+            min_r, max_r = max(min_r, Rs[j]-Ds[j]), min(max_r, Rs[j]+Ds[j])
+            min_c, max_c = max(min_c, Cs[j]-Ds[j]), min(max_c, Cs[j]+Ds[j])
+        if not (min_r <= max_r and min_c <= max_c):
+            continue
+        intersections[i] = (max_r-min_r+1)*(max_c-min_c+1)
+        for j in xrange(S):
+            if not (min_r <= Rs[j] <= max_r and
+                    min_c <= Cs[j] <= max_c):
+                continue
+            intersections[i] -= 1
+    area = [0]*len(intersections)
+    for i in xrange(1, len(area)):
+        s = i
+        while s:  # inclusion-exclusion principle
+            if COUNT_OF_ONE[s]%2:
+                area[i] += intersections[s]
+            else:
+                area[i] -= intersections[s]
+            s = (s-1)&i
+    min_p, max_p = R*C, 0
+    for i in xrange(1, len(area)):
+        count = COUNT_OF_ONE[i]
+        min_p = min(min_p, area[i]//count)  # floor(area[i]/count)
+        max_p = max(max_p, (area[-1]-area[-1-i]+count-1)//count)  # ceil((area[all]-area[all^i])/count)
+    return max_p-min_p
+
+def count_of_one(n):
+    result = 0
+    while n:
+        n &= n-1
+        result += 1
+    return result
+
+MAX_S = 15
+COUNT_OF_ONE = [0]*(2**MAX_S)
+for i in xrange(len(COUNT_OF_ONE)):
+    COUNT_OF_ONE[i] = count_of_one(i)
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, jurisdiction_restrictions())
