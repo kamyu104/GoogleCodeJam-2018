c0f53219df48e061de1e9029cb5af16cc3cb042d
Allen Liu <kamyu104@gmail.com>
2019-09-07 17:08:34 +0800

    update

--- World Finals/jurisdiction-restrictions.py
+++ World Finals/jurisdiction-restrictions.py
@@ -5,7 +5,7 @@
 #
 # Time:  O(S^6 * log(R * C)), pass in PyPy2 but Python2,
 #                             by using better but complicated max flow algorithm (Time: O(V * E)),
-#                             it can be improved to O(S^4 * log(R * C)), but Dinic is enough
+#                             it can be improved to O(S^4 * log(R * C)), but Dinic (Time: O(V^2 * E)) is enough
 # Space: O(S^2)
 #
 
