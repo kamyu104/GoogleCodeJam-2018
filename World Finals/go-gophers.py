772fe45acff687813163bf047ea187fa871dea01
Allen Liu <kamyu104@gmail.com>
2019-09-14 03:29:46 +0800

    update

--- World Finals/go-gophers.py
+++ World Finals/go-gophers.py
@@ -3,7 +3,7 @@
 # Google Code Jam 2018 World Finals - Problem C. Go, Gophers!
 # https://codingcompetitions.withgoogle.com/codejam/round/0000000000007766/000000000004da2d
 #
-# Time:  O(M * (S + (S/W)^2)), W is tuned by testing, and larger or smaller W is not necessarily good
+# Time:  O(M * (S + (S/W)^2)), W is tuned by testing, and smaller or larger W is not necessarily good
 #                            , for example, if W = 13, it uses 50% of S on average
 #                            , for another example, if W = 316, then S = (S/W)^2), O(M * (S + (S/W)^2)) = O(M * S)
 #                            , but it uses 60% of S on average and worse than that of W = 25 (10% of S)
